56e803e94b595025cb46f832ac5fe8dbf01bf526
Erwin Seribo <erwin_rulezzz87@yahoo.com>
2023-05-09 08:50:39 +0800

    added initial layout

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("on-scroll-progress").style.width = scrolled + "%";
+}
+
+$(document).ready(function() {
+  careerYears();
+
+  $(document).on('scroll', function() {
+    const nowScrollTop = $(this).scrollTop();
+
+    if (nowScrollTop > 700){
+      $('.header-content .navbar').addClass('full');
+    } else {
+      $('.header-content .navbar').removeClass('full');
+    }
+  });
+
+  $('.scroll-to').on('click', function(e) {
+    e.preventDefault();
+    const body = $('body');
+    const target = $($(this).attr('href'));
+    var position = target.offset().top - body.offset().top + body.scrollTop();
+
+    $('#bdNavbar').offcanvas('hide')
+    $('body, html').animate({ scrollTop: position });
+
+    return false;
+  });
+});
