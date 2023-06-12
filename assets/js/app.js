831000cc5761e5fa856b279438000bd608b6f299
Erwin Seribo <erwin_rulezzz87@yahoo.com>
2023-06-13 00:43:27 +0800

    initial

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,9 +7,35 @@ function myFunction() {
   document.getElementById("on-scroll-progress").style.width = scrolled + "%";
 }
 
-$(document).ready(function() {
-  careerYears();
+var countDownDate = new Date("August 6, 2023 15:00:00").getTime();
 
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML = '<div class="count"> <strong>' + days + ' </strong><span>Days</span></div> <div class="count"> <strong>' + hours + ' </strong><span> Hours</span></div> <div class="count"> <strong> '
+  + minutes + ' </strong><span>Minutes </span></div> <div class="count"> <strong> ' + seconds + ' </strong><span>Seconds</span></div>';
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+$(document).ready(function() {
   $(document).on('scroll', function() {
     const nowScrollTop = $(this).scrollTop();
 
